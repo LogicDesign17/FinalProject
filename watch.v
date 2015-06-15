1c7bb7b2d7213c6aa5237a56ceeeb3a04d0d98d0
Soochan <lsc2687@hanmail.net>
2015-06-15 16:48:03 +0900

    date 추가 (잘되지는 않음)

--- watch.v
+++ watch.v
@@ -14,17 +14,17 @@ module watch(
 	);
 
 
-	reg [2:0] mode;
+	reg [3:0] mode;
 	reg [4:0] mode_bcd;
 	reg up, down, left, right, enter, esc;
 	reg up_mark, down_mark;
 	
-	wire [2:0] norm;
-	wire [47:0] out_w [0:2];
+	wire [3:0] norm;
+	wire [47:0] out_w [0:3];
 	wire [6:0] year, month, day, hour, min, sec;
 	wire carry;
 	wire alm_w;
-	wire [5:0] blk [0:2];
+	wire [5:0] blk [0:3];
 	
 	reg [5:0] blk_on;
 	reg [47:0] blk_val;
@@ -32,7 +32,7 @@ module watch(
 	blink blinker(.on(blk_on), .val(blk_val), .clk(clk), .out(out), .reset(reset));
 
 	initial begin
-		mode = 3'b001;
+		mode = 4'b0001;
 		$monitor("mode: %b, norm: %b", mode, norm);
 	end
 	
@@ -58,30 +58,35 @@ module watch(
 				if (!up) up_mark = 0;
 				if (up && !up_mark) begin
 					mode_bcd = mode_bcd + 1;
-					mode <= {mode[1:0], mode[2]};
+					mode <= {mode[2:0], mode[3]};
 					up_mark = 1;
 				end
 				else if (down && !down_mark) begin
-					mode <= {mode[0], mode[2:1]};
+					mode <= {mode[0], mode[3:1]};
 					down_mark = 1;
 				end
 			end
 			
 			case (mode)
-				3'b001: begin
+				4'b0001: begin
 					mode_bcd = 0;
 					blk_val = out_w[0];
 					blk_on = blk[0];
 				end
-				3'b010: begin
+				4'b0010: begin
 					mode_bcd = 1;
 					blk_val = out_w[1];
 					blk_on = blk[1];
 				end
-				3'b100: begin
+				4'b0100: begin
 					mode_bcd = 2;
 					blk_val = out_w[2];
 					blk_on = blk[2];
+				end
+				4'b1000: begin
+					mode_bcd = 3;
+					blk_val = out_w[3];
+					blk_on = blk[3];
 				end
 			/*
 				6'b000001: begin
@@ -123,7 +128,7 @@ module watch(
 	
 	bcd2seven bs_mode(.in(mode_bcd), .out(out_m));
 	
-	/*
+
 	date date_m(
 		.up(up),
 		.down(down),
@@ -141,7 +146,7 @@ module watch(
 		.year(year),
 		.month(month),
 		.day(day)
-		);*/
+		);
 
 	clock clock_m(
 		.up(up),
@@ -151,12 +156,12 @@ module watch(
 		.enter(enter),
 		.esc(esc),
 		.clk(clk),
-		.mode(mode[0]),
+		.mode(mode[1]),
 		.reset(reset),
 		
-		.out(out_w[0]),
-		.blk(blk[0]),
-		.norm(norm[0]),
+		.out(out_w[1]),
+		.blk(blk[1]),
+		.norm(norm[1]),
 		.hour(hour),
 		.min(min),
 		.sec(sec),
@@ -171,13 +176,13 @@ module watch(
 		.enter(enter),
 		.esc(esc),
 		.clk(clk),
-		.mode(mode[1]),
+		.mode(mode[2]),
 		.reset(reset),
 		
-		.out(out_w[1]),
-		.blk(blk[1]),
+		.out(out_w[2]),
+		.blk(blk[2]),
 		.alm(alm_w),
-		.norm(norm[1]),
+		.norm(norm[2]),
 		.hour(hour),
 		.min(min),
 		.sec(sec)
@@ -191,12 +196,12 @@ module watch(
 		.enter(enter),
 		.esc(esc),
 		.clk(clk),
-		.mode(mode[2]),
+		.mode(mode[3]),
 		.reset(reset),
 		
-		.out(out_w[2]),
-		.blk(blk[2]),
-		.norm(norm[2])
+		.out(out_w[3]),
+		.blk(blk[3]),
+		.norm(norm[3])
 		);
 /*
 	timer timer_m(
